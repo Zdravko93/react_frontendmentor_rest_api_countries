1d722b682fb692562ceb6de9858093f595e480d0
unknown <zdravkodelic1993@gmail.com>
2025-11-18 03:00:04 +0100

    chore: wrap app with HelmetProvider for react-helmet-async

--- src/index.js
+++ src/index.js
@@ -5,14 +5,17 @@ import "./index.css";
 import App from "./App";
 
 import { AppContextProvider } from "./context/AppContext";
+import { HelmetProvider } from "react-helmet-async";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <AppContextProvider>
-      <BrowserRouter basename="/react_frontendmentor_rest_api_countries">
-        <App />
-      </BrowserRouter>
-    </AppContextProvider>
+    <HelmetProvider>
+      <AppContextProvider>
+        <BrowserRouter basename="/react_frontendmentor_rest_api_countries">
+          <App />
+        </BrowserRouter>
+      </AppContextProvider>
+    </HelmetProvider>
   </React.StrictMode>
 );
