ed924fb2de4ca740f4b4892245822654cfe4bde9
unknown <zdravkodelic1993@gmail.com>
2025-11-18 02:58:09 +0100

    feat: add SEO meta tags to Countries page

--- src/components/Countries.jsx
+++ src/components/Countries.jsx
@@ -1,4 +1,5 @@
 import { useAppContext } from "../context/AppContext";
+import { Helmet } from "react-helmet-async";
 
 import CountryCard from "./CountryCard";
 import SearchFilterGroup from "./SearchFilterGroup";
@@ -10,6 +11,29 @@ export default function Countries() {
 
   return (
     <>
+      {/* SEO */}
+      <Helmet>
+        <title>All Countries | React App</title>
+        <meta
+          name="description"
+          content="Explore countries around the world with filters and detailed information."
+        />
+        {/* Open Graph tags for social sharing  */}
+        <meta property="og:title" content="All Countries | React App" />
+        <meta
+          property="og:description"
+          content="Explore countries around the world with filters and detailed information."
+        />
+        <meta property="og:type" content="website" />
+        {/* TWITTER  */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="All Countries | React App" />
+        <meta
+          name="twitter:description"
+          content="Explore countries around the world with filters and detailed information."
+        />
+      </Helmet>
+
       <SearchFilterGroup />
       <section className={classes.countries}>
         <ul className={classes["country-list"]}>
