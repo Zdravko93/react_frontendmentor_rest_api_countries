57f54bb04ae59c9c71f9d9446e5f302bba559c4d
unknown <zdravkodelic1993@gmail.com>
2025-11-24 16:19:08 +0100

    SEO: update Open Graph and Twitter SEO image links, and update title tags

--- src/components/Countries.jsx
+++ src/components/Countries.jsx
@@ -13,24 +13,41 @@ export default function Countries() {
     <>
       {/* SEO */}
       <Helmet>
-        <title>All Countries | React App</title>
+        <title>Frontend Mentor REST API Countries | React App</title>
         <meta
           name="description"
           content="Explore countries around the world with filters and detailed information."
         />
-        {/* Open Graph tags for social sharing  */}
+
+        <link
+          rel="canonical"
+          href="https://zdravko93.github.io/react_frontendmentor_rest_api_countries/"
+        />
+
+        {/* Open Graph  */}
         <meta property="og:title" content="All Countries | React App" />
         <meta
           property="og:description"
           content="Explore countries around the world with filters and detailed information."
         />
         <meta property="og:type" content="website" />
+        <meta
+          property="og:image"
+          content="https://zdravko93.github.io/react_frontendmentor_rest_api_countries/screenshot-rest-api-countries-project.png"
+        />
         {/* TWITTER  */}
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:title" content="All Countries | React App" />
+        <meta
+          name="twitter:title"
+          content="Frontend Mentor REST API Countries | React App"
+        />
         <meta
           name="twitter:description"
           content="Explore countries around the world with filters and detailed information."
+        />
+        <meta
+          property="twitter:image"
+          content="https://zdravko93.github.io/react_frontendmentor_rest_api_countries/screenshot-rest-api-countries-project.png"
         />
       </Helmet>
 
