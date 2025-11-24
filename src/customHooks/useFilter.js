18646253af1d1c0dd10f1f2196d64ae32d7ab3f8
unknown <zdravkodelic1993@gmail.com>
2025-11-24 17:18:22 +0100

    perf: wrap handleSearchChange and handleSelectChange with useCallback inside useSearch and useFilter,  for performance

--- src/customHooks/useFilter.js
+++ src/customHooks/useFilter.js
@@ -1,11 +1,16 @@
+import { useCallback } from "react";
+
 import { useAppContext } from "../context/AppContext";
 
 export const useFilter = () => {
   const { selectedRegion, handleRegionSelect } = useAppContext();
 
-  const handleSelectChange = (event) => {
-    handleRegionSelect(event.target.value);
-  };
+  const handleSelectChange = useCallback(
+    (event) => {
+      handleRegionSelect(event.target.value);
+    },
+    [handleRegionSelect]
+  );
 
   return { selectedRegion, handleSelectChange };
 };
--- src/customHooks/useSearch.js
+++ src/customHooks/useSearch.js
@@ -1,11 +1,16 @@
+import { useCallback } from "react";
+
 import { useAppContext } from "../context/AppContext";
 
 export function useSearch() {
   const { countryInput, handleSearchCountries } = useAppContext();
 
-  const handleSearchChange = (e) => {
-    handleSearchCountries(e.target.value);
-  };
+  const handleSearchChange = useCallback(
+    (e) => {
+      handleSearchCountries(e.target.value);
+    },
+    [handleSearchCountries]
+  );
 
   return { countryInput, handleSearchChange };
 }
