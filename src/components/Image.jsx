5628fbb8cab118637ddaf4dcd794702b035b332b
unknown <zdravkodelic1993@gmail.com>
2025-11-24 17:24:20 +0100

    perf: add lazy loading for Image

--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -1,3 +1,5 @@
 export default function Image({ source, altText, className }) {
-  return <img src={source} alt={altText} className={className}></img>;
+  return (
+    <img src={source} alt={altText} className={className} loading="lazy" />
+  );
 }
